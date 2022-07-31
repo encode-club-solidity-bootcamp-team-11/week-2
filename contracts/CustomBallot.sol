1efdf15273ecc68b9f506ebf94ef7c2f2e9e3177
Samnang Chhun <samnang.chhun@gmail.com>
2022-07-31 16:23:31 +0700

    Add NatSpec documentation

--- contracts/CustomBallot.sol
+++ contracts/CustomBallot.sol
@@ -5,7 +5,9 @@ interface IERC20Votes {
     function getPastVotes(address, uint256) external view returns (uint256);
 }
 
+/// @title A Ballot contract integrates ERC20 tokens as voting power.
 contract CustomBallot {
+    /// @notice The event will be notified when a voter voted on a proposal.
     event Voted(
         address indexed voter,
         uint256 indexed proposal,
@@ -13,15 +15,20 @@ contract CustomBallot {
         uint256 proposalVotes
     );
 
+    /// @notice It represents a proposal in the ballot contract.
     struct Proposal {
         bytes32 name;
         uint256 voteCount;
     }
 
+    /// @notice Collection of proposals for voters to choose.
     Proposal[] private _proposals;
+    /// @notice The ERC20 token address that represents voting power.
     IERC20Votes private _voteToken;
+    /// @notice The block number is used as snapshot when the ballot deployed.
     uint256 private _referenceBlock;
 
+    /// @notice Track each voter's spent voting power.
     mapping(address => uint256) private _spentVotePower;
 
     constructor(bytes32[] memory proposalNames, address voteToken) {
@@ -33,14 +40,21 @@ contract CustomBallot {
         }
     }
 
+    /// @notice Get a proposal
+    /// @param index the index of the proposal collection.
     function proposals(uint256 index) external view returns(Proposal memory) {
         return _proposals[index];
     }
 
+    /// @notice Get number of spent voting power of a voter
+    /// @param voter a voter's address
     function spentVotePower(address voter) external view returns(uint256) {
         return _spentVotePower[voter];
     }
 
+    /// @notice Vote on a proposal.
+    /// @param proposal the index of porposal in the proposal collection.
+    /// @param amount the number of voting power to vote on this proposal.
     function vote(uint256 proposal, uint256 amount) external {
         uint256 votingPowerAvailable = votingPower();
         require(votingPowerAvailable >= amount, "Has not enough voting power");
@@ -51,10 +65,12 @@ contract CustomBallot {
         emit Voted(msg.sender, proposal, amount, _proposals[proposal].voteCount);
     }
 
+    /// @notice Get the winner proposal title.
     function winnerName() external view returns (bytes32) {
         return _proposals[winningProposal()].name;
     }
 
+    /// @notice Get the winner proposal index.
     function winningProposal() public view returns (uint256 winningProposal_) {
         uint256 winningVoteCount = 0;
         for (uint256 i = 0; i < _proposals.length; i++) {
@@ -65,6 +81,7 @@ contract CustomBallot {
         }
     }
 
+    /// @notice Get the remaining voting power of the caller.
     function votingPower() public view returns (uint256) {
         return (
             _voteToken.getPastVotes(msg.sender, _referenceBlock) -
--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,7 +6,10 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
+/// @title An ERC20 payment token for purchasing voting power from Ballot contract.
+/// @notice ERC20 + AccessControl + ERC20Votes implementation from OpenZeppelin.
 contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
+    /// @notice The deployer will be granted `MINTER_ROLE` to allow minting tokens.
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
@@ -14,6 +17,9 @@ contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
+    /// @notice Mint tokens to the recipient address. Only allow to call if the caller has `MINTER_ROLE`.
+    /// @param to The recipient address that will be received tokens.
+    /// @param amount The number of tokens to be minted.
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
