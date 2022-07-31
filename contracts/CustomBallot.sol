6ee21688bf3143286cadf62dc40a4759c049ee4e
Samnang Chhun <samnang.chhun@gmail.com>
2022-07-31 15:30:52 +0700

    Code reviews
    1. Change indentation to 4 spaces
    2. Re-order code layout
    3. All state variables should be private and they should be prefix by `_`

--- contracts/CustomBallot.sol
+++ contracts/CustomBallot.sol
@@ -1,61 +1,71 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.9;
 
 interface IERC20Votes {
-  function getPastVotes(address, uint256) external view returns (uint256);
+    function getPastVotes(address, uint256) external view returns (uint256);
 }
 
 contract CustomBallot {
-  event Voted(
-    address indexed voter,
-    uint256 indexed proposal,
-    uint256 weight,
-    uint256 proposalVotes
-  );
+    event Voted(
+        address indexed voter,
+        uint256 indexed proposal,
+        uint256 weight,
+        uint256 proposalVotes
+    );
 
-  struct Proposal {
-    bytes32 name;
-    uint256 voteCount;
-  }
-
-  mapping (address => uint256) public spentVotePower;
-
-  Proposal[] public proposals;
-  IERC20Votes public voteToken;
-  uint256 public referenceBlock;
-
-  constructor(bytes32[] memory proposalNames, address _voteToken) {
-    for (uint256 i = 0; i < proposalNames.length; i++) {
-      proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
+    struct Proposal {
+        bytes32 name;
+        uint256 voteCount;
     }
-    voteToken = IERC20Votes(_voteToken);
-    referenceBlock = block.number;
-  }
 
-  function vote(uint256 proposal, uint256 amount) external {
-    uint256 votingPowerAvailable = votingPower();
-    require(votingPowerAvailable >= amount, "Has not enough voting power");
-    spentVotePower[msg.sender] += amount;
-    proposals[proposal].voteCount += amount;
-    emit Voted(msg.sender, proposal, amount, proposals[proposal].voteCount);
-  }
+    Proposal[] private _proposals;
+    IERC20Votes private _voteToken;
+    uint256 private _referenceBlock;
 
-  function winningProposal() public view returns (uint256 winningProposal_) {
-    uint256 winningVoteCount = 0;
-    for (uint256 i = 0; i < proposals.length; i++) {
-      if (proposals[i].voteCount > winningVoteCount) {
-        winningVoteCount = proposals[i].voteCount;
-        winningProposal_ = i;
-      }
+    mapping(address => uint256) private _spentVotePower;
+
+    constructor(bytes32[] memory proposalNames, address voteToken) {
+        for (uint256 i = 0; i < proposalNames.length; i++) {
+            _proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
+        }
+        _voteToken = IERC20Votes(voteToken);
+        _referenceBlock = block.number;
     }
-  }
 
-  function winnerName() external view returns (bytes32) {
-    return proposals[winningProposal()].name;
-  }
+    function proposals(uint256 index) external view returns(Proposal memory) {
+        return _proposals[index];
+    }
 
-  function votingPower() public view returns (uint256) {
-    return voteToken.getPastVotes(msg.sender, referenceBlock) - spentVotePower[msg.sender];
-  }
+    function spentVotePower(address voter) external view returns(uint256) {
+        return _spentVotePower[voter];
+    }
+
+    function vote(uint256 proposal, uint256 amount) external {
+        uint256 votingPowerAvailable = votingPower();
+        require(votingPowerAvailable >= amount, "Has not enough voting power");
+        _spentVotePower[msg.sender] += amount;
+        _proposals[proposal].voteCount += amount;
+        emit Voted(msg.sender, proposal, amount, _proposals[proposal].voteCount);
+    }
+
+    function winnerName() external view returns (bytes32) {
+        return _proposals[winningProposal()].name;
+    }
+
+    function winningProposal() public view returns (uint256 winningProposal_) {
+        uint256 winningVoteCount = 0;
+        for (uint256 i = 0; i < _proposals.length; i++) {
+            if (_proposals[i].voteCount > winningVoteCount) {
+                winningVoteCount = _proposals[i].voteCount;
+                winningProposal_ = i;
+            }
+        }
+    }
+
+    function votingPower() public view returns (uint256) {
+        return (
+            _voteToken.getPastVotes(msg.sender, _referenceBlock) -
+            _spentVotePower[msg.sender]
+        );
+    }
 }
--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -7,29 +6,39 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes{
-  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-  constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
-    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    _grantRole(MINTER_ROLE, msg.sender);
-  }
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
 
-  function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-    _mint(to, amount);
-  }
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
 
-  // The following functions are overrides required by Solidity.
+    // The following functions are overrides required by Solidity.
 
-  function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
-    super._afterTokenTransfer(from, to, amount);
-  }
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
 
-  function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
-    super._mint(to, amount);
-  }
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
 
-  function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
-    super._burn(account, amount);
-  }
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
 }
